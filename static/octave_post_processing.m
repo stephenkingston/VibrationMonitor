f2fae9b166e41b6ca1ddfdb25b83150b6f8dc783
Stephen Kingston <stephen.psk@gmail.com>
2021-03-20 22:36:45 +0530

    Simple FFT computation

--- static/octave_post_processing.m
+++ static/octave_post_processing.m
@@ -1,12 +1,15 @@
 raw_row = csvread("record_channel3.csv");
 readings_as_column = transpose(raw_row);
 
-sampling_frequency = 1666.67;
+sampling_frequency = 400;
 time_period = 1/sampling_frequency;
-[number_of_readings, no_] = size(readings_as_column);
-t = (0:number_of_readings-1)*time_period;
 
-figure(1);
-plot(t, raw_row)
-figure(2);
-plot(t, fft(raw_row))
+dt = 0:time_period:2-time_period
+
+[number_of_readings, no_] = size(readings_as_column);
+nfft2 = 2^nextpow2(number_of_readings)
+ff = fft(raw_row, nfft2)
+
+% Final Fourier transform
+fff = ff(1:nfft2/2)
+plot(abs(fff))
